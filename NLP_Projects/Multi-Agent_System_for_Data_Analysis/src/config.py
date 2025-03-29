329d505f8bea725e80ee6b02d08890b0fc0c81fe
Ragiri Himadeep <142022337+HimadeepRagiri@users.noreply.github.com>
2025-03-29 11:44:37 +0530

    NLP Projects Folder

--- NLP_Projects/Multi-Agent_System_for_Data_Analysis/src/config.py
+++ NLP_Projects/Multi-Agent_System_for_Data_Analysis/src/config.py
@@ -1,7 +1,15 @@
 from transformers import BitsAndBytesConfig
 import torch
+import os
+from dotenv import load_dotenv
 
-HF_TOKEN = "Replace with your actual token"
+# Load environment variables from the .env file
+load_dotenv()
+
+# Retrieve your token securely from environment variables
+HF_TOKEN = os.getenv("HF_TOKEN")
+
+# Model
 MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.2"
 
 # Enable GPU if available
